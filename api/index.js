13895db905df999c2942df99377a3c54a3d1f96a
Dmitry <dmitry.khalimov@gmail.com>
2022-07-23 17:46:37 +0300

    Добавляет авторизацию пользователя

--- api/index.js
+++ api/index.js
@@ -7,11 +7,27 @@ const dotenv = require("dotenv");
 const helmet = require("helmet");
 const morgan = require("morgan");
 
+const userRoute = require("./routes/users");
+const authRoute = require("./routes/auth");
+
 dotenv.config();
 
-mongoose.connect(process.env.MONGO_URL, {useNewUrlParser: true}, () => {
+mongoose.connect('mongodb://localhost:27017/social', {useNewUrlParser: true}, () => {
+    // console.log(db);
     console.log("Connected to MongoDB");
 });
+
+
+app.use(express.json());
+app.use(helmet());
+app.use(morgan('common'));
+
+app.use("/api/users", userRoute);
+app.use("/api/auth", authRoute);
+
+app.get('/', (req, res) => {
+    res.send("welcome to homepage");
+})
 
 app.listen(8000, () => {
     console.log("Backend server is running");
